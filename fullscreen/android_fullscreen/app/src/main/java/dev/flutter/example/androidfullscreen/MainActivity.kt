7d91035ab121233e3cac1558f2185405118c20b1
changfeng <changfeng@gaoding.com>
2021-06-09 19:19:42 +0800

    implements go to fullscreen flutter page from android page

---
+++ fullscreen/android_fullscreen/app/src/main/java/dev/flutter/example/androidfullscreen/MainActivity.kt
@@ -0,0 +1,21 @@
+package dev.flutter.example.androidfullscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.launch_button)
+        button.setOnClickListener {
+            startActivity(
+                FlutterActivity.createDefaultIntent(this)
+            )
+        }
+    }
+}
